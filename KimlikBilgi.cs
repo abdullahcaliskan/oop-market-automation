a0fa407fbf9de399f161e36a11bb267fc8907d0a
Abdullah CALISKAN <abdullahcaliskan511@gmail.com>
2016-12-20 20:28:51 +0300

    Calisanlara ozel yaptigimiz kimlik bilgi sinifi, aggregation with kisi class

---
+++ KimlikBilgi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjeFinal
+{
+    public class KimlikBilgi
+    {
+        public Int64 TCNo { get; set; }
+        public DateTime DogumTarihi { get; set; }
+        public string KanGrubu { get; set; }
+        public bool EvliMi { get; set; }
+    }
+}
