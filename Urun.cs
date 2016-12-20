65fb81ebfdf080a3965764b64d4aef466d3072b8
Abdullah CALISKAN <abdullahcaliskan511@gmail.com>
2016-12-20 20:31:34 +0300

    Urunler class

---
+++ Urun.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjeFinal
+{
+    public enum eBirim
+    {
+        kilogram = 1,
+        adet = 2
+    };
+    public class Urun
+    {
+        // Composition
+        public UrunTanimi UrunBilgi = new UrunTanimi();
+        // Aggregation
+        public Tedarikci tedarikci;
+        public int Miktar { get; set; }
+        public decimal Fiyat { get; set; }
+        public eBirim Birim { get; set; }
+
+        public string BilgiGoster()
+        {
+            return ("Adı : " + this.UrunBilgi.Ad + Environment.NewLine +
+                    "BarkodNo : " + this.UrunBilgi.BarkodNo + Environment.NewLine +
+                    "Miktar : " + this.Miktar + Environment.NewLine +
+                    "Birim : " + this.Birim + Environment.NewLine +
+                    "Fiyat : " + this.Fiyat + Environment.NewLine +
+                    "Ek Açıklama : " + this.UrunBilgi.EkAciklama);
+        }
+    }
+}
