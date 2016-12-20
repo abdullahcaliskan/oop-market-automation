c17d17e9e8d5483ea0fd5740436826384d9dc72f
Abdullah CALISKAN <abdullahcaliskan511@gmail.com>
2016-12-20 20:28:26 +0300

    Adres bilgilerini tutan class

---
+++ AdresBilgi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjeFinal
+{
+    public class AdresBilgi
+    {
+        public string Il { get; set; }
+        public string Ilce { get; set; }
+        public string AcikAdres { get; set; }
+        public Int16 PostaKodu { get; set; }
+    }
+}
