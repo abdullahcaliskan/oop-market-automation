901e891c23cbf03cca56907d35ccc03df4cff0e9
Abdullah CALISKAN <abdullahcaliskan511@gmail.com>
2016-12-20 20:31:48 +0300

    Urunun detayli bilgileri ve barkod

---
+++ UrunTanimi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjeFinal
+{
+    public class UrunTanimi
+    {
+        // Aggregation
+        public UreticiFirma Uretici;
+        public string Ad { get; set; }
+        public int BarkodNo { get; set; }
+        public DateTime SKT { get; set; }
+        public string EkAciklama { get; set; }
+        // TODO : Enum Kategori eklenecek
+    }
+}
