2f188f061e8fcdec9326567543e35a6f7188c366
Abdullah CALISKAN <abdullahcaliskan511@gmail.com>
2016-12-20 20:27:31 +0300

    Kisi soyut sinifinin baslangici gelistirimi

---
+++ Kisi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjeFinal
+{
+    public abstract class Kisi
+    {
+        // Aggregation
+        public KimlikBilgi Kimlik;
+        // Composition
+        public AdresBilgi Adres = new AdresBilgi();
+        public string AdSoyad { get; set; }
+        public Int64 TelefonNo { get; set; }
+        public string Email { get; set; }
+        public DateTime KayitTarihi { get; set; }
+    }
+}
