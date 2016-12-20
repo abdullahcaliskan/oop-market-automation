b85972dbd67e0a8818d0654c60e34d275c5611b6
Abdullah CALISKAN <abdullahcaliskan511@gmail.com>
2016-12-20 20:30:37 +0300

    UreticiFirmada kurumsal dunyada bir kisidir, Kalitim with Kisi class

---
+++ UreticiFirma.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjeFinal
+{
+    public class UreticiFirma : Kisi
+    {
+        public Int64 VDNo { get; set; }
+    }
+}
