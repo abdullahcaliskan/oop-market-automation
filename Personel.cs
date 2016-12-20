7c7141651d52d739411d733112b88015e1eba2d0
Abdullah CALISKAN <abdullahcaliskan511@gmail.com>
2016-12-20 20:29:52 +0300

    Personel listesinin tutulabilmesi icin sinif

---
+++ Personel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjeFinal
+{
+    public class Personel : Kisi
+    {
+        public Int64 SGKNo { get; set; }
+        public int PersonelNo { get; set; }
+        public decimal Maas { get; set; }
+        public DateTime GirisTarihi { get; set; }
+    }
+}
