9f15371165e24d25119594b0e9cf394d4140d404
Abdullah CALISKAN <abdullahcaliskan511@gmail.com>
2016-12-20 20:29:20 +0300

    Kisi sinifindan kalitimla olsutrulmus tedarikci class

---
+++ Tedarikci.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjeFinal
+{
+    public class Tedarikci : Kisi
+    {
+        public int TedarikciNo { get; set; }
+    }
+}
