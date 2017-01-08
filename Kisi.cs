841a9e80dd080aef23184394452f755030172692
Abdullah <abdullahcaliskan511@gmail.com>
2017-01-09 00:01:23 +0300

    Abstract class yapıldı. Virtual ve abstract methodlar eklendi

--- Kisi.cs
+++ Kisi.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjeFinal
+namespace OOP_Okul
 {
     public abstract class Kisi
     {
@@ -16,5 +16,25 @@ namespace ProjeFinal
         public Int64 TelefonNo { get; set; }
         public string Email { get; set; }
         public DateTime KayitTarihi { get; set; }
+        protected string info;
+
+        public string Info
+        {
+            get { return info; }
+        }
+
+        public abstract void BilgiGoster();
+        public virtual int Validation()
+        {
+            if (AdSoyad == "" || TelefonNo == 0 || Email == "" || Kimlik.TCNo == 0 || Adres.AcikAdres == "")
+            {
+                this.info = "Alanların doldurulması gerekmektedir." + Environment.NewLine;
+                return (1);
+            }
+            return (0);
+                
+        }
+
+        
     }
 }
