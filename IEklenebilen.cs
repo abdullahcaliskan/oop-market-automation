75c51539d10fbb7e4990be9c66907932fb8dae2b
Abdullah <abdullahcaliskan511@gmail.com>
2017-01-09 00:59:24 +0300

    Ekle methodu için interface

---
+++ IEklenebilen.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Okul
+{
+    public interface IEklenebilen
+    {
+        string ListeEkle<T>(List<T> Param_List, T Param_Val);
+    }
+}
