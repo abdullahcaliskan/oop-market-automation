6a6be1db749b1e964cc63a7e3bfc3484fcd44238
Abdullah <abdullahcaliskan511@gmail.com>
2017-01-09 00:19:20 +0300

    Tedarikçi kişisi için gerekli abstract methodlar implemente edildi.

--- Tedarikci.cs
+++ Tedarikci.cs
@@ -4,10 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjeFinal
+
+namespace OOP_Okul
 {
     public class Tedarikci : Kisi
     {
         public int TedarikciNo { get; set; }
+        public override void BilgiGoster()
+        {
+            this.info = "TedarikciNo:" + TedarikciNo + Environment.NewLine +
+                        " AdSoyad:" + AdSoyad + Environment.NewLine +
+                        " Email:" + Email + Environment.NewLine +
+                        " TelefonNo:" + TelefonNo + Environment.NewLine +
+                        " Adres:" + Adres.AdresConcat();
+        }
+        public override int Validation()
+        {
+            base.Validation();
+            if (TedarikciNo == 0)
+            {
+                this.info += "Tedarikçi için gerekli bilgileri giriniz.";
+                return (1);
+            }
+            return (0);
+        }
+
     }
 }
