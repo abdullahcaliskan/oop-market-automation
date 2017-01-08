9958f3920b86cf2ed128769f1ccbf464d42cb394
Abdullah <abdullahcaliskan511@gmail.com>
2017-01-09 00:02:29 +0300

    Kisi için abstract methodlar implemente edildi.

--- Musteri.cs
+++ Musteri.cs
@@ -4,11 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjeFinal
+namespace OOP_Okul
 {
     public class Musteri : Kisi
     {
         public int MusteriNo { get; set; }
         public int MusteriPuan { get; set; }
+
+        public override void BilgiGoster()
+        {
+            this.info = "MüşteriNo:" + MusteriNo + Environment.NewLine +
+                        " Müşteri Puan:"+ MusteriPuan + Environment.NewLine +
+                        " AdSoyad:" + AdSoyad + Environment.NewLine +
+                        " Email:" + Email + Environment.NewLine +
+                        " TelefonNo:" + TelefonNo + Environment.NewLine +
+                        " Adres:" + Adres.AdresConcat();
+        }
+
+        public override int Validation()
+        {
+            base.Validation();
+            if (MusteriNo == 0 || MusteriPuan == 0)
+            {
+
+                this.info += "Müşteri için gerekli bilgileri giriniz.";
+                return 1;
+            }
+            return 0;
+                
+        }
     }
 }
