69b8882cc684c5beff51dc5f583d1b746692501c
Abdullah <abdullahcaliskan511@gmail.com>
2017-01-09 00:59:43 +0300

    Kopyalama methodu için interface

---
+++ IKopyalanabilir.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Okul
+{
+    public interface IKopyalanabilir
+    {
+        object Kopyala();
+    }
+}
