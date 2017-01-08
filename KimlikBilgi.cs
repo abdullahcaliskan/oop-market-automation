82eaf627aa24f98b71c636de20e23fbb3d11adf6
Abdullah <abdullahcaliskan511@gmail.com>
2017-01-09 00:00:19 +0300

    Cinsiyet ve medeni hal enum list eklendi. Nitelikler güncellendi

--- KimlikBilgi.cs
+++ KimlikBilgi.cs
@@ -4,13 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjeFinal
+namespace OOP_Okul
 {
+    public enum Cinsiyet
+    {
+        Bay,
+        Bayan
+    }
+    public enum MedeniHal
+    {
+        Evli,
+        Bekar
+    }
     public class KimlikBilgi
     {
         public Int64 TCNo { get; set; }
         public DateTime DogumTarihi { get; set; }
         public string KanGrubu { get; set; }
-        public bool EvliMi { get; set; }
+        public MedeniHal medeniHal{ get; set; }
+        public Cinsiyet cinsiyet { get; set; }
     }
 }
