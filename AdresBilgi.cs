c508ceba10991cf32a186f21a3a28a10c0e8305c
Abdullah <abdullahcaliskan511@gmail.com>
2017-01-08 23:59:56 +0300

    Method ve nitelikler güncellendi. AdresConcat eklendi.

--- AdresBilgi.cs
+++ AdresBilgi.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjeFinal
+namespace OOP_Okul
 {
     public class AdresBilgi
     {
         public string Il { get; set; }
         public string Ilce { get; set; }
         public string AcikAdres { get; set; }
-        public Int16 PostaKodu { get; set; }
+        public int PostaKodu { get; set; }
+
+        public string AdresConcat()
+        {
+            string full_address = "";
+            full_address = this.AcikAdres + " " + this.Ilce + " " + this.Il + " " + this.PostaKodu;
+            return (full_address);
+        }
     }
 }
