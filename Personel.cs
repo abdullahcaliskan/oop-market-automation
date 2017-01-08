3618d5cea75f2d5da9744a0eb2cdab03234a62fe
Abdullah <abdullahcaliskan511@gmail.com>
2017-01-09 00:12:15 +0300

    Personel kişisi için gerekli abstract methodlar implemente edildi.

--- Personel.cs
+++ Personel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjeFinal
+namespace OOP_Okul
 {
     public class Personel : Kisi
     {
@@ -12,5 +12,28 @@ namespace ProjeFinal
         public int PersonelNo { get; set; }
         public decimal Maas { get; set; }
         public DateTime GirisTarihi { get; set; }
+
+        public override void BilgiGoster()
+        {
+            this.info = "SGKNo:" + SGKNo + Environment.NewLine +
+                        " Personel No:" + PersonelNo + Environment.NewLine +
+                        " Maaş:" + Maas + Environment.NewLine +
+                        " GirisTarihi:" + GirisTarihi.ToString() + Environment.NewLine +
+                        " AdSoyad:" + AdSoyad + Environment.NewLine +
+                        " TelefonNO:" + TelefonNo + Environment.NewLine +
+                        " Email:" + Email + Environment.NewLine +
+                        " Adres:" + Adres.AdresConcat();
+        }
+        public override int Validation()
+        {
+            base.Validation();
+            if (SGKNo == 0 || PersonelNo == 0 || Maas == 0)
+            {
+                this.info += "Personel için gerekli bilgiler girilmelidir.";
+                return 1;
+            }
+            return 0;
+                
+        }
     }
 }
