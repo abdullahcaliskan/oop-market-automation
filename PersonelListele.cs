2a39ea1bcec04b3146349845f0b353069adf98b8
Abdullah <abdullahcaliskan511@gmail.com>
2017-01-13 23:15:45 +0300

    Personellerin listeden okunup grid'e yazılması

---
+++ PersonelListele.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Okul
+{
+    public partial class PersonelListele : Form
+    {
+        Market market_glob;
+
+        public PersonelListele(Market market_param)
+        {
+            market_glob = market_param;
+            InitializeComponent();
+        }
+
+        private void PersonelListele_Load(object sender, EventArgs e)
+        {
+            gridMusteriler = market_glob.PersonelListele(gridMusteriler);
+        }
+
+        private void gridMusteriler_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+
+        }
+    }
+}
