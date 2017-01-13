d896bfd615dc7f2c6ae9cf3dc00430e1f530d95d
Abdullah <abdullahcaliskan511@gmail.com>
2017-01-13 23:20:15 +0300

    Tedarikçinin listelenmesi design code

---
+++ TedarikciEkle.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace OOP_Okul
+{
+    partial class TedarikciEkle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAd = new System.Windows.Forms.Label();
+            this.lblTelefon = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblKayitTarihi = new System.Windows.Forms.Label();
+            this.lblIl = new System.Windows.Forms.Label();
+            this.lblIlce = new System.Windows.Forms.Label();
+            this.lblAcikAdres = new System.Windows.Forms.Label();
+            this.lblPostaKodu = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtIl = new System.Windows.Forms.TextBox();
+            this.dateKayitTarihi = new System.Windows.Forms.DateTimePicker();
+            this.txtIlce = new System.Windows.Forms.TextBox();
+            this.txtAcikAdres = new System.Windows.Forms.TextBox();
+            this.txtPostaKodu = new System.Windows.Forms.TextBox();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Location = new System.Drawing.Point(12, 9);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(25, 13);
+            this.lblAd.TabIndex = 0;
+            this.lblAd.Text = "İsim";
+            // 
+            // lblTelefon
+            // 
+            this.lblTelefon.AutoSize = true;
+            this.lblTelefon.Location = new System.Drawing.Point(12, 35);
+            this.lblTelefon.Name = "lblTelefon";
+            this.lblTelefon.Size = new System.Drawing.Size(60, 13);
+            this.lblTelefon.TabIndex = 1;
+            this.lblTelefon.Text = "Telefon No";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(80, 6);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(192, 20);
+            this.txtAd.TabIndex = 0;
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(80, 32);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(192, 20);
+            this.txtTelefon.TabIndex = 1;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(12, 61);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(36, 13);
+            this.lblEmail.TabIndex = 4;
+            this.lblEmail.Text = "E-Mail";
+            // 
+            // lblKayitTarihi
+            // 
+            this.lblKayitTarihi.AutoSize = true;
+            this.lblKayitTarihi.Location = new System.Drawing.Point(12, 87);
+            this.lblKayitTarihi.Name = "lblKayitTarihi";
+            this.lblKayitTarihi.Size = new System.Drawing.Size(59, 13);
+            this.lblKayitTarihi.TabIndex = 5;
+            this.lblKayitTarihi.Text = "Kayıt Tarihi";
+            // 
+            // lblIl
+            // 
+            this.lblIl.AutoSize = true;
+            this.lblIl.Location = new System.Drawing.Point(12, 113);
+            this.lblIl.Name = "lblIl";
+            this.lblIl.Size = new System.Drawing.Size(12, 13);
+            this.lblIl.TabIndex = 6;
+            this.lblIl.Text = "İl";
+            // 
+            // lblIlce
+            // 
+            this.lblIlce.AutoSize = true;
+            this.lblIlce.Location = new System.Drawing.Point(12, 139);
+            this.lblIlce.Name = "lblIlce";
+            this.lblIlce.Size = new System.Drawing.Size(24, 13);
+            this.lblIlce.TabIndex = 7;
+            this.lblIlce.Text = "İlçe";
+            // 
+            // lblAcikAdres
+            // 
+            this.lblAcikAdres.AutoSize = true;
+            this.lblAcikAdres.Location = new System.Drawing.Point(12, 165);
+            this.lblAcikAdres.Name = "lblAcikAdres";
+            this.lblAcikAdres.Size = new System.Drawing.Size(58, 13);
+            this.lblAcikAdres.TabIndex = 8;
+            this.lblAcikAdres.Text = "Açık Adres";
+            // 
+            // lblPostaKodu
+            // 
+            this.lblPostaKodu.AutoSize = true;
+            this.lblPostaKodu.Location = new System.Drawing.Point(12, 228);
+            this.lblPostaKodu.Name = "lblPostaKodu";
+            this.lblPostaKodu.Size = new System.Drawing.Size(62, 13);
+            this.lblPostaKodu.TabIndex = 9;
+            this.lblPostaKodu.Text = "Posta Kodu";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(80, 58);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(192, 20);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // txtIl
+            // 
+            this.txtIl.Location = new System.Drawing.Point(80, 110);
+            this.txtIl.Name = "txtIl";
+            this.txtIl.Size = new System.Drawing.Size(192, 20);
+            this.txtIl.TabIndex = 5;
+            // 
+            // dateKayitTarihi
+            // 
+            this.dateKayitTarihi.Location = new System.Drawing.Point(80, 84);
+            this.dateKayitTarihi.Name = "dateKayitTarihi";
+            this.dateKayitTarihi.Size = new System.Drawing.Size(192, 20);
+            this.dateKayitTarihi.TabIndex = 4;
+            // 
+            // txtIlce
+            // 
+            this.txtIlce.Location = new System.Drawing.Point(80, 136);
+            this.txtIlce.Name = "txtIlce";
+            this.txtIlce.Size = new System.Drawing.Size(192, 20);
+            this.txtIlce.TabIndex = 6;
+            // 
+            // txtAcikAdres
+            // 
+            this.txtAcikAdres.Location = new System.Drawing.Point(80, 162);
+            this.txtAcikAdres.Multiline = true;
+            this.txtAcikAdres.Name = "txtAcikAdres";
+            this.txtAcikAdres.Size = new System.Drawing.Size(192, 57);
+            this.txtAcikAdres.TabIndex = 7;
+            // 
+            // txtPostaKodu
+            // 
+            this.txtPostaKodu.Location = new System.Drawing.Point(80, 225);
+            this.txtPostaKodu.Name = "txtPostaKodu";
+            this.txtPostaKodu.Size = new System.Drawing.Size(192, 20);
+            this.txtPostaKodu.TabIndex = 8;
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Location = new System.Drawing.Point(176, 251);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(96, 23);
+            this.btnTemizle.TabIndex = 11;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(80, 251);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(90, 23);
+            this.btnEkle.TabIndex = 10;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // TedarikciEkle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 292);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.txtPostaKodu);
+            this.Controls.Add(this.txtAcikAdres);
+            this.Controls.Add(this.txtIlce);
+            this.Controls.Add(this.dateKayitTarihi);
+            this.Controls.Add(this.txtIl);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblPostaKodu);
+            this.Controls.Add(this.lblAcikAdres);
+            this.Controls.Add(this.lblIlce);
+            this.Controls.Add(this.lblIl);
+            this.Controls.Add(this.lblKayitTarihi);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtTelefon);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.lblTelefon);
+            this.Controls.Add(this.lblAd);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "TedarikciEkle";
+            this.Text = "Tedarikçi Ekle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.Label lblTelefon;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtTelefon;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblKayitTarihi;
+        private System.Windows.Forms.Label lblIl;
+        private System.Windows.Forms.Label lblIlce;
+        private System.Windows.Forms.Label lblAcikAdres;
+        private System.Windows.Forms.Label lblPostaKodu;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtIl;
+        private System.Windows.Forms.DateTimePicker dateKayitTarihi;
+        private System.Windows.Forms.TextBox txtIlce;
+        private System.Windows.Forms.TextBox txtAcikAdres;
+        private System.Windows.Forms.TextBox txtPostaKodu;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.Button btnEkle;
+    }
+}
