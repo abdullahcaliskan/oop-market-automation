9fbe2409ef91af544581e3df7978b27b7616771b
Abdullah <abdullahcaliskan511@gmail.com>
2017-01-26 19:49:14 +0300

    Combobox value değerlerinin object deger dondurmesi için ozel class

---
+++ ComboboxItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Okul
+{
+    public class ComboboxItem
+    {
+        public string Text { get; set; }
+        public Tedarikci Value { get; set; }
+
+        public override string ToString()
+        {
+            return this.Text;
+        }
+    }
+}
