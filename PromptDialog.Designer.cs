d7ac0e8b2310d9a2fa5ad8a4549cd50eb54e9714
Abdullah <abdullahcaliskan511@gmail.com>
2017-01-26 19:50:43 +0300

    Prompt penceresi tasarımı

---
+++ PromptDialog.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace OOP_Okul
+{
+    partial class PromptDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAdet = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnCikar = new System.Windows.Forms.Button();
+            this.lblBilgi = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtAdet
+            // 
+            this.txtAdet.Location = new System.Drawing.Point(15, 53);
+            this.txtAdet.Name = "txtAdet";
+            this.txtAdet.Size = new System.Drawing.Size(200, 20);
+            this.txtAdet.TabIndex = 0;
+            this.txtAdet.TextChanged += new System.EventHandler(this.txtAdet_TextChanged);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(15, 92);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(61, 23);
+            this.btnEkle.TabIndex = 3;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(149, 92);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(66, 23);
+            this.btnSil.TabIndex = 4;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnCikar
+            // 
+            this.btnCikar.Location = new System.Drawing.Point(82, 92);
+            this.btnCikar.Name = "btnCikar";
+            this.btnCikar.Size = new System.Drawing.Size(61, 23);
+            this.btnCikar.TabIndex = 5;
+            this.btnCikar.Text = "Çıkar";
+            this.btnCikar.UseVisualStyleBackColor = true;
+            this.btnCikar.Click += new System.EventHandler(this.btnCikar_Click);
+            // 
+            // lblBilgi
+            // 
+            this.lblBilgi.AutoSize = true;
+            this.lblBilgi.Location = new System.Drawing.Point(12, 20);
+            this.lblBilgi.Name = "lblBilgi";
+            this.lblBilgi.Size = new System.Drawing.Size(0, 13);
+            this.lblBilgi.TabIndex = 6;
+            // 
+            // PromptDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(227, 127);
+            this.Controls.Add(this.lblBilgi);
+            this.Controls.Add(this.btnCikar);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtAdet);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "PromptDialog";
+            this.Text = "PromptDialog";
+            this.Load += new System.EventHandler(this.PromptDialog_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAdet;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnCikar;
+        private System.Windows.Forms.Label lblBilgi;
+    }
+}
