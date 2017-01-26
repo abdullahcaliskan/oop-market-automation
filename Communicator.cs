501f1d64eac77241c3047f314dc5c722a0e6a8eb
Abdullah <abdullahcaliskan511@gmail.com>
2017-01-26 19:49:48 +0300

    2 form arasında etkileşimi sağlayıp veri aktaran ozel class

---
+++ Communicator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Okul
+{
+    public class Communicator
+    {
+        public string Response;
+        public int Miktar;
+        public string InfoMessage;
+        public int PromptMiktar;
+    }
+}
